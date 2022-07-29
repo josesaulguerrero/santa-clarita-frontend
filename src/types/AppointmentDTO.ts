f67c22fc8d76042fdc29e2947681e9fc393f9a6c
Jose Saúl Guerrero Serrano <josesaulserrano15@gmail.com>
2022-07-29 09:10:51 -0500

    Completed the todos on AppointmentDTO.ts

--- src/types/AppointmentDTO.ts
+++ src/types/AppointmentDTO.ts
@@ -1,10 +1,12 @@
+import { PartialSpecialtyDTO } from './SpecialtyDTO';
+
 export interface PartialAppointmentDTO {
 	readonly id: number;
 	readonly date: string;
 }
 
 export interface DetailedAppointmentDTO extends PartialAppointmentDTO {
-	// readonly specialty: PartialSpecialtyDTO; //TODO create specialty dto
+	readonly specialty: PartialSpecialtyDTO;
 }
 
 export interface CreateAppointmentDTO {
