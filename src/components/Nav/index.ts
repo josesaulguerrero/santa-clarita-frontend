e881ac7e669b313a2dcf575c80ab6766ecb98b25
Jose Saúl Guerrero Serrano <josesaulserrano15@gmail.com>
2022-07-29 16:06:31 -0500

    Fixed a font-size problem on the main.css

--- src/components/Nav/index.ts
+++ src/components/Nav/index.ts
@@ -1,16 +1,17 @@
 import { Component } from '../../types/common/Component';
+import './index.css';
 
 interface IProps {
 	items: string[];
 }
 
 const renderItems = (items: string[]) =>
-	items.map((item) => `<li>${item}</li>`);
+	items.map((item) => `<li>${item}</li>`).join('');
 
 export const Nav: Component<IProps> = (props): string =>
 	`
       <nav class="nav-bar">
-         <ul>
+         <ul class="nav-links">
             ${renderItems(props.items)}
          </ul>
       </nav>
